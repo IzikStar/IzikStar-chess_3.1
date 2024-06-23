5bd7bad2c91859dee6916a9d371425ff5280a510
itschak shteren <itschakme@gmail.com>
2024-06-23 11:24:43 +0300

    הוספתי הזזת כלי ללא גרירת עכבר. הוספתי צביעה של המשבצת של הכלי שנבחר

--- src/main/Board.java
+++ src/main/Board.java
@@ -5,6 +5,8 @@ import pieces.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Board extends JPanel {
 
@@ -18,6 +20,9 @@ public class Board extends JPanel {
     public CheckScanner checkScanner = new CheckScanner(this);
 
     public int enPassantTile = -1;
+
+    private boolean isWhiteToMove = true;
+    private boolean isGameOver = false;
 
     public Board() {
         this.setPreferredSize(new Dimension(cols * tileSize, rows * tileSize));
@@ -52,26 +57,26 @@ public class Board extends JPanel {
     }
 
     public void addPieces() {
-        pieceList.add(new Knight(this, 1,0, false));
-        pieceList.add(new Knight(this, 6,0, false));
-        pieceList.add(new Knight(this, 1,7, true));
-        pieceList.add(new Knight(this, 6,7, true));
+        pieceList.add(new Knight(this, 1, 0, false));
+        pieceList.add(new Knight(this, 6, 0, false));
+        pieceList.add(new Knight(this, 1, 7, true));
+        pieceList.add(new Knight(this, 6, 7, true));
 
-        pieceList.add(new Rook(this, 0,0, false));
-        pieceList.add(new Rook(this, 7,0, false));
-        pieceList.add(new Rook(this, 0,7, true));
-        pieceList.add(new Rook(this, 7,7, true));
+        pieceList.add(new Rook(this, 0, 0, false));
+        pieceList.add(new Rook(this, 7, 0, false));
+        pieceList.add(new Rook(this, 0, 7, true));
+        pieceList.add(new Rook(this, 7, 7, true));
 
-        pieceList.add(new Bishop(this, 2,0, false));
-        pieceList.add(new Bishop(this, 5,0, false));
-        pieceList.add(new Bishop(this, 2,7, true));
-        pieceList.add(new Bishop(this, 5,7, true));
+        pieceList.add(new Bishop(this, 2, 0, false));
+        pieceList.add(new Bishop(this, 5, 0, false));
+        pieceList.add(new Bishop(this, 2, 7, true));
+        pieceList.add(new Bishop(this, 5, 7, true));
 
-        pieceList.add(new King(this, 4,0, false));
-        pieceList.add(new King(this, 4,7, true));
+        pieceList.add(new King(this, 4, 0, false));
+        pieceList.add(new King(this, 4, 7, true));
 
-        pieceList.add(new Queen(this, 3,0, false));
-        pieceList.add(new Queen(this, 3,7, true));
+        pieceList.add(new Queen(this, 3, 0, false));
+        pieceList.add(new Queen(this, 3, 7, true));
 
         for (int i = 0; i < 8; i++) {
             pieceList.add(new Pawn(this, i, 1, false));
@@ -92,11 +97,20 @@ public class Board extends JPanel {
 
         // paint highLights
         if (selectedPiece != null) {
+            if (selectedPiece.isWhite == isWhiteToMove) {
+                g2d.setColor(new Color(0, 0, 255, 128)); // כחול חצי שקוף
+                g2d.fillRect(selectedPiece.col * tileSize, selectedPiece.row * tileSize, tileSize, tileSize);
+            }
             for (int c = 0; c < cols; c++) {
                 for (int r = 0; r < rows; r++) {
                     if (isValidMove(new Move(this, selectedPiece, c, r))) {
-                        g2d.setColor(new Color(85, 241, 85, 215));
-                        g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+                        if (this.getPiece(c, r) == null) {
+                            g2d.setColor(new Color(64, 227, 64, 215));
+                            g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+                        } else {
+                            g2d.setColor(new Color(238, 7, 7, 111)); // אדום חצי שקוף
+                            g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+                        }
                     }
                 }
             }
@@ -106,6 +120,7 @@ public class Board extends JPanel {
         for (Piece piece : pieceList) {
             piece.paint(g2d);
         }
+
     }
 
     public void makeMove(Move move) {
@@ -123,15 +138,23 @@ public class Board extends JPanel {
         move.piece.isFirstMove = false;
         capture(move.captured);
 
+        isWhiteToMove = !isWhiteToMove;
 
+        updateGameState();
     }
 
     public void capture(Piece piece) {
         pieceList.remove(piece);
     }
 
-    public boolean isValidMove (Move move) {
+    public boolean isValidMove(Move move) {
 
+        if (isGameOver) {
+            return false;
+        }
+        if (move.piece.isWhite != isWhiteToMove) {
+            return false;
+        }
         if (sameTeam(move.piece, move.captured)) {
             return false;
         }
@@ -148,7 +171,7 @@ public class Board extends JPanel {
         return true;
     }
 
-    public boolean sameTeam(Piece p1, Piece p2){
+    public boolean sameTeam(Piece p1, Piece p2) {
         if (p1 == null || p2 == null) {
             return false;
         }
@@ -165,7 +188,7 @@ public class Board extends JPanel {
             if (move.piece.col < move.newCol) {
                 rook = getPiece(7, move.piece.row);
                 rook.col = 5;
-            } else  {
+            } else {
                 rook = getPiece(0, move.piece.row);
                 rook.col = 3;
             }
@@ -200,5 +223,32 @@ public class Board extends JPanel {
         capture(move.piece);
     }
 
+    private void updateGameState() {
+        Piece king = findKing(isWhiteToMove);
+        if (checkScanner.isGameOver(king)) {
+            if (checkScanner.isKingChecked(new Move(this, king, king.col, king.row))) {
+                System.out.println(isWhiteToMove ? "black wins!" : "white wins!");
+            } else {
+                System.out.println("stale mate! draw!");
+            }
+            isGameOver = true;
+        } else if (insufficientMaterial(true) && insufficientMaterial(false)) {
+            System.out.println("insufficientMaterial! draw!");
+            isGameOver = true;
+        }
+
+    }
+
+    private boolean insufficientMaterial(boolean isWhite) {
+        ArrayList<String> names = pieceList.stream()
+                .filter(p -> p.isWhite == isWhite)
+                .map(p -> p.name)
+                .collect(Collectors.toCollection(ArrayList::new));
+        if (names.contains("Queen") || names.contains("Rook") || names.contains("Pawn")) {
+            return false;
+        }
+        return names.size() < 3;
+    }
 
 }
+
--- src/main/Input.java
+++ src/main/Input.java
@@ -2,6 +2,7 @@ package main;
 
 import pieces.Piece;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -11,6 +12,8 @@ import java.awt.event.MouseMotionListener;
 public class Input extends MouseAdapter {
 
     Board board;
+    private boolean isDragged = false;
+    private int selectedX = -1, selectedY = -1;
 
     public Input(Board board) {
         this.board = board;
@@ -19,48 +22,136 @@ public class Input extends MouseAdapter {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        int col = e.getX() / board.tileSize;
-        int row = e.getY() / board.tileSize;
+        if (selectedX == -1 && selectedY == -1) {
+            int col = e.getX() / board.tileSize;
+            int row = e.getY() / board.tileSize;
+            selectedX = col;
+            selectedY = row;
+            Piece pieceXY = board.getPiece(col, row);
+            if (pieceXY != null) {
+                board.selectedPiece = pieceXY;
+                // System.out.println("נבחר כלי");
+            }
+            else {
+                selectedX = -1;
+                selectedY = -1;
+            }
+            board.repaint();
+        }
+        else {
+            int col = e.getX() / board.tileSize;
+            int row = e.getY() / board.tileSize;
+            if (board.selectedPiece != null) {
+                Move move = new Move(board, board.selectedPiece, col, row);
 
-        Piece pieceXY = board.getPiece(col, row);
-        if(pieceXY != null) {
-            board.selectedPiece = pieceXY;
+                if (board.isValidMove(move)) {
+                    board.makeMove(move);
+                    // System.out.println("מזיז את הכלי לנקודה שנבחרה");
+                    selectedX = -1;
+                    selectedY = -1;
+                }
+                else {
+                    board.selectedPiece.xPos = board.selectedPiece.col * board.tileSize;
+                    board.selectedPiece.yPos = board.selectedPiece.row * board.tileSize;
+                }
+
+                board.selectedPiece = null;
+                board.repaint();
+            }
         }
     }
 
-
-
     @Override
     public void mouseDragged(MouseEvent e) {
-
         if (board.selectedPiece != null) {
+            // System.out.println("גרירת עכבר.");
             board.selectedPiece.xPos = e.getX() - board.tileSize / 2;
             board.selectedPiece.yPos = e.getY() - board.tileSize / 2;
 
             board.repaint();
+            isDragged = true;
         }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        if (isDragged) {
+            // System.out.println("מניח את הכלי בנקודה אליה הוא נגרר.");
+            int col = e.getX() / board.tileSize;
+            int row = e.getY() / board.tileSize;
+            if (board.selectedPiece != null) {
+                Move move = new Move(board, board.selectedPiece, col, row);
 
-        int col = e.getX() / board.tileSize;
-        int row = e.getY() / board.tileSize;
-
-        if (board.selectedPiece != null) {
-            Move move = new Move(board, board.selectedPiece, col, row);
-
-            if (board.isValidMove(move)) {
-                board.makeMove(move);
+                if (board.isValidMove(move)) {
+                    board.makeMove(move);
+                }
+                else {
+                    board.selectedPiece.xPos = board.selectedPiece.col * board.tileSize;
+                    board.selectedPiece.yPos = board.selectedPiece.row * board.tileSize;
+                }
+                board.selectedPiece = null;
+                board.repaint();
             }
-            else {
-                board.selectedPiece.xPos = board.selectedPiece.col * board.tileSize;
-                board.selectedPiece.yPos = board.selectedPiece.row * board.tileSize;
-            }
-
-            board.selectedPiece = null;
-            board.repaint();
+            selectedX = -1;
+            selectedY = -1;
+            isDragged = false;
         }
+//        else {
+//            System.out.println("לא עושה כלום. לא היתה גרירת עכבר.");
+//        }
     }
+
+//    private void handleSquareClick(int x, int y) {
+//        Piece selectedPiece = board.getPiece(x, y);
+//
+//        if (selectedX == -1 && selectedY == -1) {
+//            if (selectedPiece != null && selectedPiece.isWhite == boardState.isWhiteTurn) {
+//                selectedX = x;
+//                selectedY = y;
+//                Point[] possibleMoves = selectedPiece.showAllPossibleMoves(boardState, true);
+//                legalMoves.clear();
+//                attackMoves.clear();
+//                for (Point move : possibleMoves) {
+//                    if (boardState.getPiece(move.x, move.y) != null) {
+//                        attackMoves.add(move);
+//                    } else {
+//                        legalMoves.add(move);
+//                    }
+//                }
+//            }
+//        } else {
+//            if (board.movePiece(selectedX, selectedY, x, y, true)) {
+//                selectedX = -1;
+//                selectedY = -1;
+//                legalMoves.clear();
+//                attackMoves.clear();
+//            } else {
+//                if (selectedPiece != null && selectedPiece.isBlack() != boardState.isWhiteTurn) {
+//                    selectedX = x;
+//                    selectedY = y;
+//                    Point[] possibleMoves = selectedPiece.showAllPossibleMoves(boardState, true);
+//                    legalMoves.clear();
+//                    attackMoves.clear();
+//                    for (Point move : possibleMoves) {
+//                        if (boardState.getPiece(move.x, move.y) != null) {
+//                            attackMoves.add(move);
+//                        } else {
+//                            legalMoves.add(move);
+//                        }
+//                    }
+//                } else {
+//                    selectedX = -1;
+//                    selectedY = -1;
+//                    legalMoves.clear();
+//                    attackMoves.clear();
+//                }
+//            }
+//        }
+//        updateChessBoard();
+//        if (isStatusChanged) {
+//            // בדיקת מצב המשחק לאחר הצביעה מחדש
+//            SwingUtilities.invokeLater(() -> boardState.checkGameState(isCheckMate, isStaleMate, isWhiteTurn));
+//        }
+//    }
 
 }
